5baf5ea8749c9094f4fa5c7a288489f03d38cac6
Diego Silva Costa <137888485+DiegoCostaDiego@users.noreply.github.com>
2024-05-23 11:37:54 -0300

    Add files via upload

---
+++ DesafioPOO/Models/Smartphone.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        protected string Modelo;
+        protected string IMEI;
+        protected int Memoria;
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo Ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
---
+++ DesafioPOO/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia= new Nokia(numero: "98877-5544", modelo: "Nokia N1", imei: "1111111111", memoria: 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Notion");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "97788-5566", modelo: "Iphone 29", imei: "222222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
